ecf938ebcaef717033c5393247f3efaf8417d66d
RijalRifus <rijalrifus17@gmail.com>
2020-04-23 21:12:28 +0700

    push

---
+++ blog/blogPostOne.md
@@ -0,0 +1,7 @@
+---
+title: Blog Post One
+---
+
+# Blog Post One
+
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio rerum maiores harum modi cumque, asperiores possimus dolorem illo! Sunt ab placeat ad labore ipsam dolores odio commodi repellendus maiores sint.
---
+++ src/pages/blogHtml/blogPostThree.vue
@@ -0,0 +1,16 @@
+<template>
+    <BlogLayout>
+        <h1>Blog three</h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio rerum maiores harum modi cumque, asperiores possimus dolorem illo! Sunt ab placeat ad labore ipsam dolores odio commodi repellendus maiores sint.
+        </p>
+    </BlogLayout>
+</template>
+
+<script>
+export default {
+    metaInfo: {
+        title: 'Blog Three '
+    }
+}
+</script>
